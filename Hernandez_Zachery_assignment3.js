6338169d976d849422775f884db2af47e2fc9753
Zachery Hernandez <Zak@Zacherys-MacBook-Pro.local>
2012-03-12 22:20:50 -0500

    Updated HTML and CSS files (just annotating) and added in first object into JS file (following modular pattern).

--- Hernandez_Zachery_assignment3.js
+++ Hernandez_Zachery_assignment3.js
@@ -1,1 +1,9 @@
-alert("JavaScript works!");
+var deviceSpecs = function(name) {
+	var getName = function() { return name};
+	return {
+		"name": getName,
+	};
+};
+var bold = deviceSpecs("BlackBerry Bold 9700");
+console.log(bold.name());
+console.log(deviceSpecs.name);
