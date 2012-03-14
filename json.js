a83c37b8fcc8b53d0ee58887ee5052329288b953
Zachery Hernandez <Zak@Zacherys-MacBook-Pro.local>
2012-03-13 20:53:04 -0500

    Added JSON file and updated HTML and CSS files (just annotating).

---
+++ json.js
@@ -0,0 +1,23 @@
+/*
+Zachery Hernandez
+March 15, 2012
+Deliverable 3
+BlackBerry, Themes, SVG
+Public Git Repository at: https://github.com/AlucardFair/SDI-Project-3
+*/
+
+var otherDevices = "sameSpecs" [
+	//follow same resolution //
+	{
+		"name": ["Tour 9630", "Bold 9650", "Curve 8900"],
+		"os": ["5.0", "5.0", "5.0"]
+	};
+	var differentDevices = "" [
+	//do not follow same resolution //
+	{
+		"differentName": ["Curve 8300", "Curve 9300", "Storm 2 9550"],
+		"differentOs": ["4.5", "6.0", "4.7.1"]
+	};
+	
+	];
+];
